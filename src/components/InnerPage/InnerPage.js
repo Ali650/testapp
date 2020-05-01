6632e3707420e0aabeebd4116f66f6a969646653
Ahmed Ali <thunderstruck772@gmail.com>
2020-05-01 21:12:38 +0300

    test

--- src/components/InnerPage/InnerPage.js
+++ src/components/InnerPage/InnerPage.js
@@ -12,13 +12,13 @@ const innerPage = ()=> {
             This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. This is page 2 description. 
             </p>
             <div className="social-icons">
-                <a target="_blank" href="http://www.facebook.com">
+                <a href="https://www.facebook.com" alt="facebook link">
                     <img className="social-icon facebook" src={facebook} alt=""/>
                 </a>
-                <a target="_blank" href="http://www.twitter.com">
+                <a href="https://www.twitter.com" alt="twitter link">
                     <img className="social-icon twitter" src={twitter} alt=""/>
                 </a>
-                <a target="_blank" href="http://www.instagram.com">
+                <a href="https://www.instagram.com" alt="instagram link">
                     <img className="social-icon instagram" src={Instagram} alt=""/>
                 </a>
             </div>
