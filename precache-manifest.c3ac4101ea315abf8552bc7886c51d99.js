760936530824d665324b83d858999685773d84fb
Ahmed Ali <thunderstruck772@gmail.com>
2020-05-01 06:34:59 +0300

    Updates

---
+++ precache-manifest.c3ac4101ea315abf8552bc7886c51d99.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3789d17399552fa3aafd6ffeb31895ff",
+    "url": "/testapp/index.html"
+  },
+  {
+    "revision": "3f709ad3cdb84fea1686",
+    "url": "/testapp/static/css/main.77f8f0e5.chunk.css"
+  },
+  {
+    "revision": "cc4d1bb7ac0a3cc953a5",
+    "url": "/testapp/static/js/2.d2966530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testapp/static/js/2.d2966530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f709ad3cdb84fea1686",
+    "url": "/testapp/static/js/main.fc64c134.chunk.js"
+  },
+  {
+    "revision": "cdfc787d41c314a80081",
+    "url": "/testapp/static/js/runtime-main.78574772.js"
+  },
+  {
+    "revision": "3f06be062ad59c4d6f4b2a7d6694776f",
+    "url": "/testapp/static/media/JRE.3f06be06.jpg"
+  },
+  {
+    "revision": "7614cba68e436191b584e5b36e7447bb",
+    "url": "/testapp/static/media/MFM.7614cba6.jpg"
+  },
+  {
+    "revision": "05628f795dd0d8502174d3bcea761175",
+    "url": "/testapp/static/media/Play.05628f79.svg"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/testapp/static/media/SFPro.d09549c1.ttf"
+  },
+  {
+    "revision": "d1cfb499c3caa686f3265953f490824c",
+    "url": "/testapp/static/media/User.d1cfb499.jpg"
+  },
+  {
+    "revision": "79f8177efb84655fb8a48d19c8da482b",
+    "url": "/testapp/static/media/cloud.79f8177e.svg"
+  },
+  {
+    "revision": "70aaf7cfc6625f9da8e4c317707f4d7c",
+    "url": "/testapp/static/media/download.70aaf7cf.svg"
+  },
+  {
+    "revision": "b9d74a68e2b250009af34173e21ab145",
+    "url": "/testapp/static/media/headphones.b9d74a68.svg"
+  },
+  {
+    "revision": "84e1dff738b306f411efbfc77e3e16f4",
+    "url": "/testapp/static/media/liked.84e1dff7.svg"
+  },
+  {
+    "revision": "5ee6a65681f1a9b22106a218bcd0bea4",
+    "url": "/testapp/static/media/mic.5ee6a656.svg"
+  },
+  {
+    "revision": "0a2eceacf685f5e495e48b61e454dcf9",
+    "url": "/testapp/static/media/played.0a2eceac.svg"
+  },
+  {
+    "revision": "49379e702c20d22425a057e987083833",
+    "url": "/testapp/static/media/refresh.49379e70.svg"
+  }
+]);
